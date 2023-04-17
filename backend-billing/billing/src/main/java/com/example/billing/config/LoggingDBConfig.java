c7619f9dcb7759739c707fa0c02396aed288c061
이상훈 <akqjqdmltn@naver.com>
2023-04-17 18:04:21 +0900

    🎉BE/Config [S08P31A702-49] : Working on DB configuration

---
+++ backend-billing/billing/src/main/java/com/example/billing/config/LoggingDBConfig.java
@@ -0,0 +1,64 @@
+package com.example.billing.config;
+
+import org.springframework.boot.autoconfigure.context.ConfigurationPropertiesAutoConfiguration;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.hibernate.SpringImplicitNamingStrategy;
+import org.springframework.boot.orm.jpa.hibernate.SpringPhysicalNamingStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(
+        entityManagerFactoryRef = "loggingEntityManager",
+        transactionManagerRef = "loggingTransactionManager",
+        basePackages = "com.example.billing.data.repository"
+)
+public class LoggingDBConfig {
+
+    private final ConfigurationPropertiesAutoConfiguration ConfigurationProperties;
+
+    public BillingDBConfig(ConfigurationPropertiesAutoConfiguration ConfigurationProperties) {
+        this.ConfigurationProperties = ConfigurationProperties;
+    }
+
+    @Bean
+    @ConfigurationProperties("spring.datasource.hikari.bootdb2")
+    public DataSource legacyDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    public PlatformTransactionManager legacyTransactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(legacyEntityManager().getObject());
+
+        return transactionManager;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean legacyEntityManager() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(legacyDataSource());
+        em.setPackagesToScan("com.jiyoon.model.legacy");
+
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(adapter);
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("hibernate.physical_naming_strategy", SpringPhysicalNamingStrategy.class.getName());
+        properties.put("hibernate.implicit_naming_strategy", SpringImplicitNamingStrategy.class.getName());
+        em.setJpaPropertyMap(properties);
+
+        return em;
+    }
+}
