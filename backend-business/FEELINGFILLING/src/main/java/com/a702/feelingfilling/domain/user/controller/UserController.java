742cdc4fc51525d9dbcafa988db7e7f5a5110691
고명진 <mgin0527@naver.com>
2023-04-18 14:06:26 +0900

    :sparkles: BE/Feat [S08P31A702-42] Add User domain
    유저관리 패키지 설정 및 User Entity,Dto 추가

--- backend-business/FEELINGFILLING/src/main/java/com/a702/feelingfilling/domain/user/controller/UserController.java
+++ backend-business/FEELINGFILLING/src/main/java/com/a702/feelingfilling/domain/user/controller/UserController.java
@@ -1,5 +1,13 @@
 package com.a702.feelingfilling.domain.user.controller;
 
-public class UserController {
+import com.a702.feelingfilling.domain.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class UserController {
+	@Autowired
+	public UserService userService;
+	
+	
 }
---
+++ backend-business/FEELINGFILLING/src/main/java/com/a702/feelingfilling/domain/user/service/UserServiceImpl.java
@@ -0,0 +1,7 @@
+package com.a702.feelingfilling.domain.user.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService{
+}
