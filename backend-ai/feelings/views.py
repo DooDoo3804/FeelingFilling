39607d953e98fd940f0b2319b6ea5546bdbbfb07
dodo327 <ddodo327@naver.com>
2023-04-19 15:39:42 +0900

    ✨ BE/Feat gpu 가속 함수 추가

--- backend-ai/feelings/views.py
+++ backend-ai/feelings/views.py
@@ -1,13 +1,37 @@
+import tensorflow as tf
 from django.shortcuts import render
 from transformers import pipeline
+from googletrans import Translator
 
-# Create your views here.
 
-def analysis_text(request) :
-    
+# 텍스트 번역 api
+# post 요청
+def analysis_text(request):
     pass
 
 
-
-def analysis_voice(request) :
+def analysis_voice(request):
     pass
+
+
+# GPU 가속 함수
+def acc_gpu():
+    # GPU 사용 가능한지 확인
+    print("GPU is", "available" if tf.config.list_physical_devices(
+        "GPU") else "NOT AVAILABLE")
+    # GPU 가속 활성화
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(
+                logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            print(e)
+
+
+acc_gpu()
