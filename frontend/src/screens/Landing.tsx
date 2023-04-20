551d9f9eb850fc3dead8dde2d8fff1f35ba05250
ebing <ebing.salmon@gmail.com>
2023-04-20 11:17:49 +0900

    ✨ FE/Feat : 랜딩 페이지에 로고 추가

--- frontend/src/screens/Landing.tsx
+++ frontend/src/screens/Landing.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
-import {Text} from 'react-native';
+import styled from 'styled-components/native';
+import {Common} from '../components/Common';
+import font_logo from '../assets/font_logo.png';
+
+const Container = styled.View`
+  flex: 1;
+  flex-direction: column;
+  background-color: ${Common.colors.white01};
+  align-items: center;
+  padding: 10px;
+`;
+
+const FontLogo = styled.Image`
+  width: 200px;
+  height: 200px;
+`;
 
 const Landing = () => {
-  return <Text>Landing</Text>;
+  return (
+    <Container>
+      <FontLogo source={font_logo} />
+    </Container>
+  );
 };
 
 export default Landing;
