3ae109d2925ed2fe0029ebc4a3ac02190a8fcd4b
김성환 <fanngineer@gmail.com>
2023-04-20 14:49:43 +0900

    :sparkles: BE/Feat [S08P31A702-35] : Create Chatting Class
    Chatting Controller, Service 객체 생성

---
+++ backend-business/FEELINGFILLING/src/main/java/com/a702/feelingfilling/domain/chatting/controller/ChattingController.java
@@ -0,0 +1,14 @@
+package com.a702.feelingfilling.domain.chatting.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("chatting")
+@Slf4j
+public class ChattingController {
+
+}
