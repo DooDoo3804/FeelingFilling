e99654a3a7af20ff8b96f10f74274b081643f0cd
dodo327 <ddodo327@naver.com>
2023-04-20 14:10:13 +0900

    ✨ BE/Feat gpu 가속 함수 활성화

--- backend-ai/feelings/views.py
+++ backend-ai/feelings/views.py
@@ -57,24 +57,24 @@ def analysis_emition(translation_result):
 
 
 # GPU 가속 함수
-# def acc_gpu():
-#     # GPU 사용 가능한지 확인
-#     print("GPU is", "available" if tf.config.list_physical_devices(
-#         "GPU") else "NOT AVAILABLE")
-#     # GPU 가속 활성화
-#     gpus = tf.config.experimental.list_physical_devices('GPU')
-#     if gpus:
-#         try:
-#             # Currently, memory growth needs to be the same across GPUs
-#             for gpu in gpus:
-#                 tf.config.experimental.set_memory_growth(gpu, True)
-#             logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-#             print(len(gpus), "Physical GPUs,", len(
-#                 logical_gpus), "Logical GPUs")
-#         except RuntimeError as e:
-#             # Memory growth must be set before GPUs have been initialized
-#             print(e)
-# acc_gpu()
+def acc_gpu():
+    # GPU 사용 가능한지 확인
+    print("GPU is", "available" if tf.config.list_physical_devices(
+        "GPU") else "NOT AVAILABLE")
+    # GPU 가속 활성화
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(
+                logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            print(e)
 
 
+acc_gpu()
 translation_text()
