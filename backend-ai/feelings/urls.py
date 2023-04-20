b685c46624dbb3e5c121888b51e19c2e5d8f9c05
dodo327 <ddodo327@naver.com>
2023-04-20 15:35:06 +0900

    ✨ BE/Feat url slash 변경

--- backend-ai/feelings/urls.py
+++ backend-ai/feelings/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('text/', views.analysis_text, name="analysis_text"),
-    path('voice/', views.analysis_voice, name="analysis_voice"),
+    path('text', views.analysis_text),
+    path('voice', views.analysis_voice),
 ]
