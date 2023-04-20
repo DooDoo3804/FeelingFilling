052f9bc8744ffce4abd4ff9f82a1d32ebf57d05a
ebing <ebing.salmon@gmail.com>
2023-04-20 10:58:31 +0900

    ✨ FE/Feat : 마이페이지 내 메뉴 컴포넌트 추가

--- frontend/src/screens/Mypage.tsx
+++ frontend/src/screens/Mypage.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components/native';
 import EntypoIcon from 'react-native-vector-icons/Entypo';
 import {Common} from '../components/Common';
 
 import emo_happy from '../assets/emo_happy.png';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
 const Container = styled.View`
   flex: 1;
   background-color: ${Common.colors.white01};
-  padding: 10px;
+  padding: 12px;
 `;
 
 const Heading = styled.Text`
   font-family: 'NotoSansKR-Bold';
   font-size: 20px;
+  margin: 5px 0px;
   margin-left: 15px;
   color: ${Common.colors.deepGrey};
 `;
@@ -25,6 +27,7 @@ const ProfileContainer = styled.TouchableOpacity`
   padding: 20px;
   justify-content: space-between;
   align-items: center;
+  elevation: 5;
 `;
 
 const ProfileWrapper = styled.View`
@@ -36,8 +39,57 @@ const EmotionImage = styled.Image`
   height: 60px;
 `;
 
+const HeadingWrapper = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 20px;
+  align-items: center;
+`;
+
+const SubHeading = styled.Text`
+  font-family: 'NotoSansKR-Bold';
+  margin-right: 15px;
+  font-size: 15px;
+  color: ${Common.colors.selectGrey};
+`;
+
+const BadgeContainer = styled.View`
+  flex-direction: row;
+  height: 80px;
+  padding: 0px 10px;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const NoBadge = styled.Text`
+  font-family: 'NotoSansKR-Regular';
+  margin-left: 15px;
+  font-size: 15px;
+  color: ${Common.colors.selectGrey};
+`;
+
+const PlainText = styled.Text`
+  font-family: 'NotoSansKR-Regular';
+  margin-left: 15px;
+  font-size: 15px;
+  color: ${Common.colors.deepGrey};
+`;
+
+const MenuList = styled.View`
+  margin: 0px 15px;
+  margin-top: 15px;
+  padding-top: 25px;
+  border-top-width: 1px;
+  border-top-color: ${Common.colors.basicGrey};
+`;
+
+const SingleMenu = styled.TouchableOpacity`
+  padding: 5px 0px;
+`;
+
 const Mypage = ({navigation}: {navigation: any}) => {
-  // 마이페이지 보여주는 화면입니다.
+  const [badges, setBadges] = useState();
+
   return (
     <Container>
       <Heading>마이페이지</Heading>
@@ -54,6 +106,30 @@ const Mypage = ({navigation}: {navigation: any}) => {
           />
         </ProfileWrapper>
       </ProfileContainer>
+      <HeadingWrapper>
+        <Heading>활동 배지</Heading>
+        <TouchableOpacity>
+          <SubHeading>더 보기</SubHeading>
+        </TouchableOpacity>
+      </HeadingWrapper>
+      <BadgeContainer>
+        {badges ? (
+          <NoBadge>뱃지얌</NoBadge>
+        ) : (
+          <NoBadge>활동 배지가 없습니다.</NoBadge>
+        )}
+      </BadgeContainer>
+      <MenuList>
+        <SingleMenu>
+          <PlainText>환급 정보 등록하기</PlainText>
+        </SingleMenu>
+        <SingleMenu>
+          <PlainText>로그아웃</PlainText>
+        </SingleMenu>
+        <SingleMenu>
+          <PlainText>회원탈퇴</PlainText>
+        </SingleMenu>
+      </MenuList>
     </Container>
   );
 };
