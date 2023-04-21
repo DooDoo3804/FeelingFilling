9c53750a43436693f5f70f01e9bbd9409e7d972e
ebing <ebing.salmon@gmail.com>
2023-04-21 15:44:41 +0900

    💄 FE/Design : swiper 내부 이미지, 글귀 설정

--- frontend/src/screens/Landing.tsx
+++ frontend/src/screens/Landing.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import {Image} from 'react-native';
 import styled from 'styled-components/native';
 import Swiper from 'react-native-swiper';
+import Lottie from 'lottie-react-native';
 
 import {Common} from '../components/Common';
 import font_logo from '../assets/font_logo.png';
 import kakao_logo from '../assets/kakao_logo.png';
+import landing01 from '../assets/landing_01.json';
 
 const Container = styled.View`
   flex: 1;
@@ -22,16 +24,23 @@ const FontLogo = styled.Image`
 `;
 
 const SwiperConatiner = styled.View`
-  height: 150px;
+  height: 220px;
 `;
 
 const SwiperView = styled.View`
+  height: 150px;
   justify-content: center;
   text-align: center;
 `;
 
 const SwiperText = styled.Text`
+  font-family: 'NotoSansKR-Medium';
   text-align: center;
+`;
+
+const LottieContainer = styled.View`
+  height: 120px;
+  margin-bottom: 10px;
 `;
 
 const LoginBtn = styled.TouchableOpacity`
@@ -64,13 +73,36 @@ const Landing = () => {
           autoplayTimeout={3}
           activeDotColor={Common.colors.selectGrey}>
           <SwiperView>
-            <SwiperText>Hello Swiper</SwiperText>
+            <LottieContainer>
+              <Lottie
+                source={require('../assets/landing_01.json')}
+                autoPlay
+                loop
+              />
+            </LottieContainer>
+            <SwiperText>감정을 다스리고, 돈도 모아보세요.</SwiperText>
           </SwiperView>
           <SwiperView>
-            <SwiperText>Beautiful</SwiperText>
+            <LottieContainer>
+              <Lottie
+                source={require('../assets/landing_02.json')}
+                autoPlay
+                loop
+              />
+            </LottieContainer>
+            <SwiperText>나의 감정 점수와 통계를 확인해요.</SwiperText>
           </SwiperView>
           <SwiperView>
-            <SwiperText>And simple</SwiperText>
+            <LottieContainer>
+              <Lottie
+                source={require('../assets/landing_03.json')}
+                autoPlay
+                loop
+              />
+            </LottieContainer>
+            <SwiperText>
+              한 달이 지나면 모은 돈을 돌려받을 수 있어요.
+            </SwiperText>
           </SwiperView>
         </Swiper>
       </SwiperConatiner>
