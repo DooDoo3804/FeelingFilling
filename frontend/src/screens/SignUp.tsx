3971ef7afaa223e7b6edc6f014372588bf223801
ebing <ebing.salmon@gmail.com>
2023-04-21 16:43:34 +0900

    ✨ FE/Feat : 회원가입 때 정보 설정하는 페이지 틀 추가하는 중

--- frontend/src/screens/SignUp.tsx
+++ frontend/src/screens/SignUp.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
-import {Text} from 'react-native';
+
+import font_logo from '../assets/font_logo.png';
+import kakao_logo from '../assets/kakao_logo.png';
+
+import {
+  Container,
+  FontLogo,
+  KakaoLogo,
+  BtnText,
+  ColorBtn,
+} from '../styles/LoginStyle';
+import {Common} from '../components/Common';
 
 const SignUp = () => {
-  return <Text>SignUp</Text>;
+  return (
+    <Container>
+      <FontLogo source={font_logo} />
+      <ColorBtn color={Common.colors.kakao}>
+        <KakaoLogo source={kakao_logo} />
+        <BtnText textColor={Common.colors.deepGrey}>결제 등록하기</BtnText>
+      </ColorBtn>
+      <ColorBtn color={Common.colors.subColor02}>
+        <BtnText textColor={Common.colors.white01}>설정 완료</BtnText>
+      </ColorBtn>
+    </Container>
+  );
 };
 
 export default SignUp;
