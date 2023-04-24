a6cda4c317359d4d54ee95d4f42302a38bcfc483
고명진 <mgin0527@naver.com>
2023-04-24 15:22:06 +0900

    :sparkles: [S08P31A702-122] BE/Feat Add getUserThisMonth
    개인 통계 중 이번 달 저금 API 구현

--- backend-business/FEELINGFILLING/src/main/java/com/a702/feelingfilling/domain/request/service/RequestService.java
+++ backend-business/FEELINGFILLING/src/main/java/com/a702/feelingfilling/domain/request/service/RequestService.java
@@ -6,6 +6,7 @@ import com.a702.feelingfilling.domain.request.model.dto.Stat;
 import java.util.List;
 
 public interface RequestService {
-
+	
+	List<Stat> getUserThisMonth(Integer userId);
 	EmotionHigh getEmotionHigh(Integer userId);
 }
