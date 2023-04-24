c4bbea34d1de5432a60af7da2c5227cbc7fa6ade
고명진 <mgin0527@naver.com>
2023-04-24 15:18:17 +0900

    :sparkles: BE/Feat Edit BadgeRepository
    badge 정보 front에서 보관하므로 BadgeRepository 수정

--- backend-business/FEELINGFILLING/src/main/java/com/a702/feelingfilling/domain/user/model/repository/BadgeRepository.java
+++ backend-business/FEELINGFILLING/src/main/java/com/a702/feelingfilling/domain/user/model/repository/BadgeRepository.java
@@ -4,5 +4,5 @@ import com.a702.feelingfilling.domain.user.model.entity.Badge;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BadgeRepository extends JpaRepository<Badge, Integer> {
-	Badge findbyBadgeId(int badgeId);
+	Badge findByUser_UserId(int userId);
 }
