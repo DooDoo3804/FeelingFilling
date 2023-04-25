9674131fefdf523a4c2100d6a86f02239cc0baf0
ebing <ebing.salmon@gmail.com>
2023-04-25 10:58:07 +0900

    ✨ FE/Feat : 회원가입 시 정보 설정하는 페이지 CSS 완료

--- frontend/src/screens/SignUp.tsx
+++ frontend/src/screens/SignUp.tsx
@@ -1,4 +1,6 @@
 import React, {useEffect, useState} from 'react';
+import {ScrollView} from 'react-native-gesture-handler';
+import {RangeSlider} from '@sharcoux/slider';
 
 import {Common} from '../components/Common';
 import font_logo from '../assets/font_logo.png';
@@ -13,12 +15,16 @@ import {
   InfoWrapper,
   InfoTitle,
   NameInput,
+  SliderContainer,
   TitleContainer,
 } from '../styles/LoginStyle';
 
 const SignUp = () => {
   const [nickname, setNickname] = useState<string>('');
   const [nameError, setNameError] = useState<boolean>(false);
+  const [multiSliderValue, setMultiSliderValue] = useState<number[]>([
+    0, 10000,
+  ]);
 
   useEffect(() => {
     if (nickname.length > 20) {
@@ -27,6 +33,10 @@ const SignUp = () => {
       setNameError(false);
     }
   }, [nickname]);
+
+  const onChange = (range: [number, number]) => {
+    setMultiSliderValue(range);
+  };
 
   return (
     <Container>
@@ -40,17 +50,40 @@ const SignUp = () => {
             </BtnText>
           )}
         </TitleContainer>
-        <NameInput onChangeText={text => setNickname(text)} />
+        <ScrollView scrollEnabled={false}>
+          <NameInput onChangeText={text => setNickname(text)} />
+        </ScrollView>
         <InfoTitle>결제 범위</InfoTitle>
+        <SliderContainer>
+          <RangeSlider
+            range={[multiSliderValue[0], multiSliderValue[1]]} // set the current slider's value
+            minimumValue={0} // Minimum value
+            maximumValue={10000} // Maximum value
+            step={500} // The step for the slider (0 means that the slider will handle any decimal value within the range [min, max])
+            minimumRange={0} // Minimum range between the two thumbs (defaults as "step")
+            crossingAllowed={false} // If true, the user can make one thumb cross over the second thumb
+            outboundColor={Common.colors.basicGrey} // The track color outside the current range value
+            inboundColor={Common.colors.emotionColor03} // The track color inside the current range value
+            thumbTintColor={Common.colors.emotionColor03} // The color of the slider's thumb
+            enabled={true} // If false, the slider won't respond to touches anymore
+            trackHeight={5} // The track's height in pixel
+            thumbSize={18} // The thumb's size in pixel
+            slideOnTap={true} // If true, touching the slider will update it's value. No need to slide the thumb.
+            onValueChange={onChange} // Called each time the value changed. The type is (range: [number, number]) => void
+            // ref={rangeSliderRef}
+          />
+          <TitleContainer>
+            <BtnText textColor={Common.colors.deepGrey}>
+              {multiSliderValue[0]}원 ~ {multiSliderValue[1]}원
+            </BtnText>
+          </TitleContainer>
+        </SliderContainer>
       </InfoWrapper>
+
       <ColorBtn color={Common.colors.kakao}>
         <KakaoLogo source={kakao_logo} />
         <BtnText textColor={Common.colors.deepGrey}>결제 등록하기</BtnText>
       </ColorBtn>
-      <ColorBtn color={Common.colors.subColor02}>
-        <BtnText textColor={Common.colors.white01}>설정 완료</BtnText>
-      </ColorBtn>
-      <BtnText textColor="#000">{nickname}</BtnText>
     </Container>
   );
 };
