157a6871d66e64b18b7d1af5db0cb0a910857c6d
ebing <ebing.salmon@gmail.com>
2023-04-27 16:25:03 +0900

    ✨ FE/Feat : 전체 통계 페이지 CSS 작업 진행중

--- frontend/src/screens/OverallStats.tsx
+++ frontend/src/screens/OverallStats.tsx
@@ -1,12 +1,171 @@
-import React from 'react';
-import {Container, ScreenScroll, TitleText} from '../styles/OverallStatsStyle';
+import React, {useEffect, useState} from 'react';
+import Lottie from 'lottie-react-native';
+import {
+  VictoryChart,
+  VictoryLabel,
+  VictoryPie,
+  VictoryScatter,
+} from 'victory-native';
+import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5';
+
+import {Common} from '../components/Common';
+import {
+  Container,
+  TitleText,
+  ScreenScroll,
+  PageContainer,
+  EmoKingContainer,
+  TextWrapper,
+  EmoLottieWrapper,
+  EmoKingText,
+} from '../styles/OverallStatsStyle';
+import {useAxios} from '../hooks/useAxios';
 
 const OverallStats = () => {
-  // 전체 통계 보여주는 화면입니다.
+  const [monthTotal, setMonthTotal] = useState([
+    {emotion: 'happy', amount: '1', x: 2, y: 0},
+    {emotion: 'sad', amount: '1', x: 0, y: 0},
+    {emotion: 'angry', amount: '1', x: 1, y: 1},
+  ]);
+
+  const [totalMoney, setTotalMoney] = useState([
+    {x: 1, y: 5, label: 'asdfasdfd원'},
+    {x: 2, y: 5, label: 'asdfasdfd원'},
+    {x: 3, y: 5, label: 'asdfasdfd원'},
+  ]);
+
+  const {data, error} = useAxios(
+    'http://k8a702.p.ssafy.io:8080/api/stat/all',
+    'GET',
+    null,
+  );
+  console.log(data);
+  console.log(error);
+
+  const priceConverter = (price: string) => {
+    return price.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  };
+
+  useEffect(() => {
+    setMonthTotal([
+      {emotion: 'anger', amount: '218342300', x: 1, y: 1},
+      {emotion: 'joy', amount: '10156730', x: 2, y: 0},
+      {emotion: 'sadness', amount: '5134820', x: 0, y: 0.2},
+    ]);
+
+    setTotalMoney([
+      {x: 1, y: 35, label: 'asdfasdfd원'},
+      {x: 2, y: 40, label: 'asdfasdfd원'},
+      {x: 3, y: 55, label: 'asdfasdfd원'},
+    ]);
+  }, []);
+
   return (
     <Container>
       <ScreenScroll>
-        <TitleText>이번 달 저금</TitleText>
+        <PageContainer>
+          <TitleText>이번 달 저금</TitleText>
+          <VictoryScatter
+            data={monthTotal}
+            style={{
+              data: {
+                fill: (
+                  {datum}, //datum은 VictoryBar에서 사용하는 데이터의 하나의 항목을 나타내는 객체
+                ) =>
+                  datum.emotion === 'angry'
+                    ? Common.colors.emotionColor01
+                    : datum.emotion === 'happy'
+                    ? Common.colors.emotionColor02
+                    : Common.colors.emotionColor03,
+              },
+            }}
+            bubbleProperty="amount"
+            maxBubbleSize={80}
+            minBubbleSize={60}
+            labels={({datum}) => priceConverter(datum.amount)}
+            // animate={{
+            //   duration: 2000,
+            //   easing: 'bounce',
+            //   onLoad: {duration: 1000},
+            // }}
+            padding={{top: 90, bottom: 120, left: 60, right: 100}}
+            labelComponent={
+              <VictoryLabel
+                style={[
+                  {
+                    fontFamily: 'NotoSansKR-Bold',
+                    fontSize: 20,
+                    dy: 20,
+                  },
+                ]}
+              />
+            }
+          />
+          <TitleText>전날 저금 추이</TitleText>
+          {/* 차트 */}
+          <EmoKingContainer>
+            <TextWrapper>
+              <EmoKingText>
+                이번달 감정 왕{'  '}
+                <FontAwesome5Icon name="crown" color="#FFE24B" size={20} />
+              </EmoKingText>
+              <EmoKingText>98회 저금</EmoKingText>
+              <EmoKingText>저금액 21,134,000원</EmoKingText>
+            </TextWrapper>
+            <EmoLottieWrapper>
+              <Lottie
+                source={require('../assets/emotion_king.json')}
+                autoPlay
+                loop
+              />
+            </EmoLottieWrapper>
+          </EmoKingContainer>
+          <TitleText>저금 총 누적액</TitleText>
+          <VictoryPie
+            data={totalMoney}
+            innerRadius={120}
+            labelRadius={({innerRadius}) => innerRadius + 5}
+            radius={80}
+            cornerRadius={10}
+            padAngle={3}
+            colorScale={[
+              Common.colors.emotionColor01,
+              Common.colors.emotionColor02,
+              Common.colors.emotionColor03,
+            ]}
+            style={{labels: {fontSize: 14, fontWeight: 'bold'}}}
+            padding={{top: -80, bottom: -50, left: 100, right: 120}}
+            animate={{
+              easing: 'exp',
+              duration: 12000,
+            }}
+            events={[
+              {
+                target: 'data',
+                eventHandlers: {
+                  onPressIn: () => {
+                    return [
+                      {
+                        target: 'data',
+                        mutation: ({style}) => {
+                          return style.fill === '#c43a31'
+                            ? null
+                            : {style: {fill: '#c43a31'}};
+                        },
+                      },
+                      // {
+                      //   target: 'data',
+                      //   mutation: ({innerRadius}) => {
+                      //     return innerRadius === 120 ? null : {innerRadius: 80};
+                      //   },
+                      // },
+                    ];
+                  },
+                },
+              },
+            ]}
+          />
+        </PageContainer>
       </ScreenScroll>
     </Container>
   );
--- frontend/src/styles/OverallStatsStyle.tsx
+++ frontend/src/styles/OverallStatsStyle.tsx
@@ -16,3 +16,35 @@ export const TitleText = styled.Text`
 export const ScreenScroll = styled.ScrollView`
   flex: 1;
 `;
+
+export const PageContainer = styled.View`
+  flex: 1;
+`;
+
+export const EmoKingContainer = styled.View`
+  background-color: ${Common.colors.chatColor02};
+  border-radius: 20px;
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px 20px;
+`;
+
+export const EmoKingText = styled.Text`
+  color: ${Common.colors.deepGrey};
+  font-size: 18px;
+  font-family: 'NotoSansKR-Bold';
+  line-height: 28px;
+`;
+
+export const TextWrapper = styled.View`
+  padding: 10px 0px;
+`;
+
+export const EmoLottieWrapper = styled.View`
+  background-color: ${Common.colors.white01};
+  width: 110px;
+  height: 110px;
+  border-radius: 100px;
+`;
