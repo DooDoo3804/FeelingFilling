5dec694f029cb3196e7368f4fc1cc124cc0a35b4
고명진 <mgin0527@naver.com>
2023-04-27 14:39:21 +0900

    :art: BE/Feat [S08P31A702-126] edit getUserMonths
    개인 통계 중 월별 추이 API 수정

--- backend-business/FEELINGFILLING/src/main/java/com/a702/feelingfilling/domain/request/service/RequestServiceImpl.java
+++ backend-business/FEELINGFILLING/src/main/java/com/a702/feelingfilling/domain/request/service/RequestServiceImpl.java
@@ -5,6 +5,7 @@ import com.a702.feelingfilling.domain.request.model.repository.RequestRepository
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,11 +14,11 @@ public class RequestServiceImpl implements RequestService{
 	
 	@Autowired
 	private RequestRepository requestRepository;
-	
+	private String[] emotion = new String[]{"anger","joy","sadness"};
 	@Override
 	public List<Stat> getUserThisMonth(Integer userId) {
 		List<StatInterface> statInterfaces = requestRepository.getUserThisMonth(userId);
-		
+
 		List<Stat> stats = new ArrayList<>();
 		statInterfaces.forEach(x -> stats.add(Stat.builder()
 				.emotion(x.getEmotion())
@@ -27,17 +28,42 @@ public class RequestServiceImpl implements RequestService{
 		return stats;
 	}
 	
+	public int getInd(int now, int m){
+		int res = 5-(now-m+12)%12;
+		return res;
+	}
 	@Override
-	public List<Month> getUserMonths(Integer userId) {
+	public Month[][] getUserMonths(Integer userId) {
 		List<MonthInterface> monthInterfaces = requestRepository.getUserMonths(userId);
+
+		int now = LocalDateTime.now().getMonthValue();
 		
-		List<Month> months = new ArrayList<>();
+		Month[][] months = new Month[3][6];
+		for(int i = 5;i>=0;i--) {
+			for(int j = 0;j<3;j++){
+				months[j][i] = Month.builder()
+						.month((now+6+i)%12+1)
+						.amount(0)
+						.build();
+			}
+		}
 		
-		monthInterfaces.forEach(x -> months.add(Month.builder()
-				.emotion(x.getEmotion())
-				.month(x.getMonth())
-				.amount(x.getAmount())
-				.build()));
+		int i = 0;
+		int j;
+
+		for(MonthInterface monthInterface: monthInterfaces){
+			while(i<3 && !monthInterface.getEmotion().equals(emotion[i]))i++;
+			j = getInd(now,monthInterface.getMonth());
+			months[i][j].setEmotion(monthInterface.getEmotion());
+			months[i][j].setAmount(monthInterface.getAmount());
+		}
+//		List<Month> months = new ArrayList<>();
+//
+//		monthInterfaces.forEach(x -> months.add(Month.builder()
+//				.emotion(x.getEmotion())
+//				.month(x.getMonth())
+//				.amount(x.getAmount())
+//				.build()));
 		
 		return months;
 	}
