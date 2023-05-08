f8e10682a5e194d3ebf7fe956c3cc4cdce487cf2
ebing <ebing.salmon@gmail.com>
2023-05-08 12:41:49 +0900

    ✨ FE/Feat [S08P31A702-217] : 결제 수단 등록 페이지 기본 틀 생성

--- frontend/src/screens/Payment.tsx
+++ frontend/src/screens/Payment.tsx
@@ -1,8 +1,31 @@
-import React from 'react';
-import {button} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {WebView} from 'react-native-webview';
 
 const Payment = () => {
-  return <button>흐흐</button>;
+  const [data, setData] = useState<JSON | null>(null);
+
+  useEffect(() => {
+    fetch('http://13.124.31.137:8702/billing/subscription/active/', {
+      method: 'POST',
+      headers: {'Content-type': 'application/json'},
+      body: JSON.stringify({
+        serviceName: 'abcd',
+        serviceUserId: 3,
+      }),
+    })
+      .then(response => response.json())
+      .then(res => {
+        console.log(res);
+        setData(res);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <>{data && <WebView originWhitelist={['*']} source={{uri: data.url}} />}</>
+  );
 };
 
 export default Payment;
