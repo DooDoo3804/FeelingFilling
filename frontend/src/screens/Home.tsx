f42e0a702e5e05cb5420e59b400dd2173aa41817
ebing <ebing.salmon@gmail.com>
2023-05-11 14:13:39 +0900

    💄 FE/Design : 광고 틀 생성

--- frontend/src/screens/Home.tsx
+++ frontend/src/screens/Home.tsx
@@ -4,6 +4,9 @@ import React, {useState} from 'react';
 // import {toggleProgress} from '../redux';
 // import type {AppState} from '../redux';
 
+import Swiper from 'react-native-swiper';
+
+import {Common} from '../components/Common';
 import {
   Container,
   Heading,
@@ -15,6 +18,13 @@ import {
   BalanceText,
   BtnText,
 } from '../styles/HomeStyle';
+
+import {
+  SwiperConatiner,
+  SwiperView,
+  SwiperText,
+  LottieContainer,
+} from '../styles/LoginStyle';
 
 const Home = ({navigation}: {navigation: any}) => {
   const [balanceView, setBalanceView] = useState(true);
@@ -49,6 +59,29 @@ const Home = ({navigation}: {navigation: any}) => {
           </BalanceBtn>
         </BtnWrapper>
       </MoneyWrapper>
+      <SwiperConatiner>
+        <Swiper
+          showsButtons={false}
+          autoplay
+          loop
+          autoplayTimeout={3}
+          activeDotColor={Common.colors.selectGrey}>
+          <SwiperView>
+            <LottieContainer></LottieContainer>
+            <SwiperText>감정을 다스리고, 돈도 모아보세요.</SwiperText>
+          </SwiperView>
+          <SwiperView>
+            <LottieContainer></LottieContainer>
+            <SwiperText>나의 감정 점수와 통계를 확인해요.</SwiperText>
+          </SwiperView>
+          <SwiperView>
+            <LottieContainer></LottieContainer>
+            <SwiperText>
+              한 달이 지나면 모은 돈을 돌려받을 수 있어요.
+            </SwiperText>
+          </SwiperView>
+        </Swiper>
+      </SwiperConatiner>
     </Container>
   );
 };
