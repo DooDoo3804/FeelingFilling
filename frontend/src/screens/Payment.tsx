920b66694a9bfdb30f18086447aadda369cbc717
ebing <ebing.salmon@gmail.com>
2023-05-17 00:40:38 +0900

    ✨ FE/Feat [S08P31A702-289] : 결제 등록 WebView로 설정
    성공 시에 닫고 로그인 처리, 오류 났을 때 처리만 하면 끝

--- frontend/src/screens/Payment.tsx
+++ frontend/src/screens/Payment.tsx
@@ -1,31 +1,61 @@
 import React, {useEffect, useState} from 'react';
-import {WebView} from 'react-native-webview';
+import {Alert} from 'react-native';
+import {WebView, WebViewNavigation} from 'react-native-webview';
+
+import {useAxiosWithRefreshToken} from '../hooks/useAxioswithRfToken';
+
+interface ApiResponse {
+  message: string;
+  url: string;
+}
 
 const Payment = ({navigation, route}: {navigation: any; route: any}) => {
-  const [data, setData] = useState<JSON | null>(null);
   const kakaoId = route.params.kakaoId.toString();
+  const [webView, setWebView] = useState<boolean>(false);
+
+  const {data, error} = useAxiosWithRefreshToken<ApiResponse>(
+    'https://feelingfilling.store/api/user/register',
+    'GET',
+    null,
+  );
+
+  const getCode = (target: WebViewNavigation) => {
+    const elements = target.url.split('/');
+    const resState = elements[elements.length - 1];
+
+    if (resState === 'success') {
+      console.log(resState, '성공');
+    } else if (resState === 'info') {
+      console.log(resState, '처리 진행중');
+    } else {
+      console.log(resState, '에러');
+      setWebView(false);
+      Alert.alert('결제 등록', '결제 등록 중 오류가 발생했습니다.', [
+        {text: '확인', onPress: () => navigation.popToTop()},
+      ]);
+    }
+  };
 
   useEffect(() => {
-    fetch('http://13.124.31.137:8702/billing/subscription/active/', {
-      method: 'POST',
-      headers: {'Content-type': 'application/json'},
-      body: JSON.stringify({
-        serviceName: 'abcd',
-        serviceUserId: 3,
-      }),
-    })
-      .then(response => response.json())
-      .then(res => {
-        console.log(res);
-        setData(res);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }, []);
+    if (data && data.message === 'SUCCESS') {
+      console.log('데이터 받아옴', data);
+      setWebView(true);
+    } else {
+      console.log(error);
+      setWebView(false);
+    }
+  }, [data, error]);
 
   return (
-    <>{data && <WebView originWhitelist={['*']} source={{uri: data.url}} />}</>
+    <>
+      {webView && data && (
+        <WebView
+          originWhitelist={['*']}
+          source={{uri: data.url}}
+          onNavigationStateChange={getCode}
+        />
+      )}
+    </>
   );
 };
 
