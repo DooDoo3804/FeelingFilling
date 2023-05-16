015ed809b2d9e21c8f2c52f119f0bdf705c2cb77
dodo327 <dodo327@naver.com>
2023-05-16 13:48:45 +0900

    ✨ BE/Feat settings 주석 수정

--- backend-ai/FEELINGFILLING_DJANGO/settings.py
+++ backend-ai/FEELINGFILLING_DJANGO/settings.py
@@ -98,19 +98,7 @@ DATABASES = {
         "PASSWORD": "mammoth77",
         "HOST": "3.38.191.128",
         "PORT": '3306'
-    },
-    # 'mongodb': {
-    #     'ENGINE': 'django_mongodb_engine',
-    #     'NAME': 'mydatabase',
-    #     'USER': '',
-    #     'PASSWORD': '',
-    #     'HOST': '3.38.191.128',
-    #     'PORT': '27017',
-    #     'OPTIONS': {
-    #         'tz_aware': True,
-    #         'connect': False,
-    #     },
-    # },
+    }
 }
 # DATABASE_ROUTERS = ['django_mongodb_engine.router.MongoDBRouter']
 
