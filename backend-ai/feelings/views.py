7e39e2e549697fcd7970963ddfb46cae3c85dec9
dodo327 <dodo327@naver.com>
2023-05-18 16:37:43 +0900

    ✨ BE/Feat dreact 답변 교육

--- backend-ai/feelings/views.py
+++ backend-ai/feelings/views.py
@@ -257,7 +257,7 @@ def cal_deposit(score, user_id):
 # GPT // ChatBot react 생성 함수
 def make_react(text):
     print(text)
-    prompt = text + ". 위로하거나 맞장구 쳐주는 말 또는 상황에 맞는 자연스러운 말을 해줘. 반말하지 말고 짧게 두 문장을 해요체로 작성해. 4문장 이상 길게 말하지 마!"
+    prompt = text + ". 위로하거나 맞장구 쳐주는 말 또는 상황에 맞는 자연스러운 말을 해줘. 반말하지 말고 짧게 두 문장을 해요체로 작성하는데 많은 문장을 말하지마"
     print(prompt)
     openai.api_key = settings.OPEN_AI_API_KEY
     # 밑에 모델 변경 가능 "text-ada-002"
