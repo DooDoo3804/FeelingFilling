b6a16f236b4d3fbca6a1ff41874480efb96391e0
dodo327 <dodo327@naver.com>
2023-05-18 14:22:15 +0900

    ✨ BE/Feat dreact 반응 수정

--- backend-ai/feelings/views.py
+++ backend-ai/feelings/views.py
@@ -257,7 +257,7 @@ def cal_deposit(score, user_id):
 # GPT // ChatBot react 생성 함수
 def make_react(text):
     print(text)
-    prompt = text + ". 위로하거나 맞장구 쳐주는 말 또는 상황에 맞는 자연스러운 두 문장으로 존대해서 ~요 로 끝나는 말로 짧게 한국말로 해줘"
+    prompt = text + ". 위로하거나 맞장구 쳐주는 말 또는 상황에 맞는 자연스러운 말을 해줘. 반말하지 말고 길면 두 문장정도 ~요 로 끝나는 문장으로 작성해"
     print(prompt)
     openai.api_key = settings.OPEN_AI_API_KEY
     # 밑에 모델 변경 가능 "text-ada-002"
